954554d05ec750c828a14287c14b9a61060e4c6d
3x6th <smashbaster@gmail.com>
2024-06-27 19:41:12 +0300

    init commit

---
+++ src/main/java/com/web3/with/WithApplication.java
@@ -0,0 +1,13 @@
+package com.web3.with;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web3/with/WithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web3.with;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
